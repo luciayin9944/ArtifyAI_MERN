ade440621986f1585330a8c34b621bc52e83233e
luciayin9944 <luxiayin94@gmail.com>
2025-10-28 14:26:06 -0700

    setup Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import BuyCredit from './pages/BuyCredit'
+import AIGenerator from './pages/AIGenerator'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>
-        Hello ArtifyAI!
-      </h1>
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/buy' element={<BuyCredit />} />
+        <Route path='/image' element={<AIGenerator />} />
+      </Routes>
     </div>
   )
 }
---
+++ client/src/pages/AIGenerator.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AIGenerator = () => {
+  return (
+    <div>AIGenerator</div>
+  )
+}
+
+export default AIGenerator
