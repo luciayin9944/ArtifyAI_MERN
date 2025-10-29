2a2b8c9a8bbb31798913073c003446b65a3c3a04
luciayin9944 <luxiayin94@gmail.com>
2025-10-29 00:47:49 -0700

    implemented NavBar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.jsx'
+import AppContextProvider from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
+
+  <BrowserRouter>
+    <AppContextProvider>
       <App />
-    </BrowserRouter>
-  </StrictMode>,
+    </AppContextProvider>
+  </BrowserRouter>
+
 )
