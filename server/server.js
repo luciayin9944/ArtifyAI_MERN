6d19dc686c2d64b326cc9215e71fe3573ed8ac45
luciayin9944 <luxiayin94@gmail.com>
2025-10-31 22:45:00 -0700

    implemented mongoDB & userModel

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
+import connectDB from './config/mongodb.js';
 
 
 const PORT = process.env.PORT || 4000;
@@ -8,7 +9,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+await connectDB();
 
 app.get('/', (req, res) => res.send("API is working"));
 
-app.listen(PORT, () => console.log('Server is running on port: ' + PORT));
+// app.listen(PORT, () => console.log('Server is running on port: ' + PORT));
+const server = app.listen(PORT, () => {
+  console.log('Server is running on port:', PORT);
+});
+server.on('error', (err) => console.error('Server error:', err));
