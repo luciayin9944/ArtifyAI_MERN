a9ef90c3176fdde07d532f8042a25891aefbd3e1
luciayin9944 <luxiayin94@gmail.com>
2025-11-05 17:59:18 -0800

    update assets

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,14 +1,10 @@
 import logo from './logo.svg'
 import logo_icon from './logo_icon.svg'
-import facebook_icon from './facebook_icon.svg'
-import instagram_icon from './instagram_icon.svg'
-import twitter_icon from './twitter_icon.svg'
 import star_icon from './star_icon.svg'
 import rating_star from './rating_star.svg'
 import sample_img_1 from './sample_img_1.png'
 import sample_img_2 from './sample_img_2.png'
-import profile_img_1 from './profile_img_1.png'
-import profile_img_2 from './profile_img_2.png'
+import sample_img_3 from './sample_img_3.png'
 import step_icon_1 from './step_icon_1.svg'
 import step_icon_2 from './step_icon_2.svg'
 import step_icon_3 from './step_icon_3.svg'
@@ -22,13 +18,11 @@ import profile_icon from './profile_icon.png'
 export const assets = {
     logo,
     logo_icon,
-    facebook_icon,
-    instagram_icon,
-    twitter_icon,
     star_icon,
     rating_star,
     sample_img_1,
     sample_img_2,
+    sample_img_3,
     email_icon,
     lock_icon,
     cross_icon,
@@ -55,29 +49,6 @@ export const stepsData = [
     },
   ];
 
-export const testimonialsData = [
-    {
-        image:profile_img_1,
-        name:'Donald Jackman',
-        role:'Graphic Designer',
-        stars:5,
-        text:`I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`
-    },
-    {
-        image:profile_img_2,
-        name:'Richard Nelson',
-        role:'Content Creator',
-        stars:5,
-        text:`I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`
-    },
-    {
-        image:profile_img_1,
-        name:'Donald Jackman',
-        role:' Graphic Designer',
-        stars:5,
-        text:`I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`
-    },
-]
 
 export const plans = [
     {
